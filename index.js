416d505921aeca8e16ad748ce0174dd72e186178
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-09 13:49:56 +0700

    Strategy

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const { getNdutConfig } = require('ndut-helper')
+const plugin = require('./lib/plugin')
+
 module.exports = async function (fastify) {
-  const options = {}
+  const name = 'ndut-auth'
+  const options = getNdutConfig(fastify, name) || {}
+  options.strategy = {
+    basic: true,
+    bearer: true,
+    jwt: true
+  }
   const dependency = ['ndut-db']
-  return { options, dependency }
+  return { name, plugin, options, dependency }
 }
--- ndutRest/hook/on-request.js
+++ ndutRest/hook/on-request.js
@@ -1,3 +1,15 @@
+const { _, getNdutConfig } = require('ndut-helper')
+
 module.exports = async function (request, reply) {
-  console.log('on request from ndutAuth')
+  const site = request.site || {}
+  const config = getNdutConfig(this, 'ndut-auth')
+  let user = null
+  try {
+    // TODO: check route needs to be authenticated or not
+    const strategy = _.get(request, 'headers.authorization', '').split(' ')[0]
+    if (_.isEmpty(strategy)) throw new Error('Authorization header is empty!')
+    if (strategy === 'Basic' && config.strategy.basic) user = await this.ndutAuth.helper.getUserByBasicAuth(request, reply)
+  } catch (err) {
+    throw this.Boom.boomify(err, { statusCode: 401 })
+  }
 }
