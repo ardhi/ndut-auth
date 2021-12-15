a9e47c2b386a00d7d7123efbaa97ed76d8ff21fc
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-15 15:07:40 +0700

    Use global wrapper

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const plugin = require('./lib/plugin')
+const crypto = require('crypto')
 
 module.exports = async function (fastify) {
+  const { fp } = fastify.ndut.helper
+  const plugin = fp(require('./lib/plugin'))
   const name = 'ndut-auth'
   const options = fastify.ndut.helper.getNdutConfig(fastify, name) || {}
   options.strategy = {
@@ -9,7 +11,7 @@ module.exports = async function (fastify) {
   }
   options.apiKeyQueryString = options.apiKeyQueryString || 'apiKey'
   options.apiKeyHeader = options.apiKeyHeader || 'X-Api-Key'
-  options.jwt = options.jwt || { key: 'Jaya!Jaya!Jaya!' }
+  options.jwt = options.jwt || { key: crypto.randomBytes(16).toString('hex') }
   options.jwt.expiresIn = options.jwt.expiresIn || (1000 * 60 * 60 * 24 * 7)
   const dependency = ['ndut-db']
   return { name, plugin, options, dependency, appModes: ['serve', 'build'] }
