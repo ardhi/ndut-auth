8c632774a317ffa6f8fd5989b5568eeb76649818
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-18 19:35:16 +0700

    Bug fix

--- ndutRest/route/profile/put.js
+++ ndutRest/route/profile/put.js
@@ -7,7 +7,7 @@ module.exports = {
     const { _ } = this.ndut.helper
     const id = request.user.id
     const body = _.omit(request.body, ['password', 'token', 'status', 'id', 'siteId'])
-    const model = this.ndutDb.helper.getModelByAlias(request.params.model)
+    const model = this.ndutDb.helper.getModelByAlias('auth-user')
     await this.ndutDb.update(model, request, { id }, body)
     const data = await this.ndutDb.findById(model, request, id)
     return {
