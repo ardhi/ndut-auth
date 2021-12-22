5b84715a18fb3c1c0e9a5457af2ca2a27e21aa56
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-22 18:39:56 +0700

    Typo

--- ndut/helper/get-user-by-username-password.js
+++ ndut/helper/get-user-by-username-password.js
@@ -5,7 +5,7 @@ module.exports = async function (username, password, siteId) {
   const where = { username, status: 'ENABLED' }
   if (aneka.isSet(siteId)) where.siteId = siteId
   const result = await this.ndutDb.model.AuthUser.findOne({ where })
-  if (!result) throw new this.Boom.Boom('Unknown user or user is disabled', { data: { username: 'unknwon' } })
+  if (!result) throw new this.Boom.Boom('Unknown user or user is disabled', { data: { username: 'unknown' } })
   const check = await verifyPassword(password, result.password)
   if (!check) throw new this.Boom.Boom('Invalid password', { data: { password: 'invalid' }})
   return result
