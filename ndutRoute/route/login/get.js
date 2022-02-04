0d5ebf683977770579592bc665f8341b1c81c9d0
Ardhi Lukianto <ardhi@lukianto.com>
2022-02-04 20:09:18 +0700

    Some i18n improvement

--- ndutRoute/route/login/get.js
+++ ndutRoute/route/login/get.js
@@ -1,4 +1,4 @@
 module.exports = async function (request, reply) {
-  if (request.user) reply.view('route:/authenticated')
+  if (request.user) reply.view('auth:/authenticated')
   else reply.view('auth:/login-form', { url: { login: request.url } })
 }
