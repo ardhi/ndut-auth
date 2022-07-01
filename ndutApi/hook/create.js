15ae9554904e606279fe3cca666c41354f3b8321
Ardhi Lukianto <ardhi@lukianto.com>
2022-07-01 07:10:19 +0700

    Updates

--- ndutApi/hook/create.js
+++ ndutApi/hook/create.js
@@ -2,8 +2,10 @@ module.exports = async function beforeCreateAuth ({ model, body = {}, filter = {
   const supported = await this.ndutAuth.helper.isUserSupportedModel(model)
   const private = await this.ndutAuth.helper.isPrivateModel(model)
   const createdBy = await this.ndutAuth.helper.isCreatedByModel(model)
+  const updatedBy = await this.ndutAuth.helper.isUpdatedByModel(model)
   const filterUserId = (filter.user || {}).id
   if (createdBy && filterUserId) body.createdBy = filterUserId
+  if (updatedBy && filterUserId) body.updatedBy = filterUserId
   if (private && filterUserId) {
     body.userId = filterUserId
     return
