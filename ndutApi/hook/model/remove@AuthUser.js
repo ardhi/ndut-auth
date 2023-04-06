9dcbbe30ad944ce64724e79f55446eb6c83234e9
Ardhi Lukianto <ardhi@lukianto.com>
2023-04-06 22:59:20 +0700

    Updates

--- ndutApi/hook/model/remove@AuthUser.js
+++ ndutApi/hook/model/remove@AuthUser.js
@@ -1,5 +1,5 @@
 module.exports = async function beforeRemoveAuth ({ model, params = {}, filter = {} }) {
   const { _ } = this.ndut.helper
   const user = await this.ndutApi.helper.dbCall({ method: 'findOne', model: 'AuthUser', params: { where: { id: params.id } } })
-  if (['admin'].includes(_.get(user, 'username'))) throw this.Boom.forbidden('cantRemoveAdmin', { ndut: 'auth' })
+  if (['admin'].includes(_.get(user, 'username', ''))) throw this.Boom.forbidden('cantRemoveAdmin', { ndut: 'auth' })
 }
